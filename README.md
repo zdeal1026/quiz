030c2cc63483721be69ec3f75300b748d1a27239
Zdeal <jedimastershake@gmail.com>
2021-06-05 00:49:57 -0400

    js file started

--- README.md
+++ README.md
@@ -17,21 +17,19 @@ timed quiz
 
 
 # Purpose
-
+The purpose of this project is to create a "Code Quiz"
 
 
 # Criteria
-1. 
+1. When the timer starts, the user is presented with a question.
 
-2. 
+2. When the question is answered, another question is presented.
 
-3. 
+3. When a question is answered incorrectly, time is subtracted from the timer.
 
-4. 
+4. When the questions are answered OR the timer reaches 0 the game is over.
 
-5. 
-
-6.  
+5. When the game is over, the user can enter initials in a scoreboard. 
 
 
 # Notes
---
+++ script.js
@@ -0,0 +1,53 @@
+// questions for the quiz
+
+var askQuestions = [
+    {
+        question: "Where in the HTML file is the CSS file placed?",
+        pickA: "Body",
+        pickB: "Footer",
+        pickC: "Head",
+        pickD: "Div",
+        answer: "c"
+    }
+    {
+        question: "What is a boolean?",
+        pickA: "A file",
+        pickB: "A data type with 2 possible choices",
+        pickC: "A numeric variable",
+        pickD: "A tag for HTML",
+        answer: "b"
+    }
+    {
+        question: "How many body elements can be in one HTML file?",
+        pickA: "2",
+        pickB: "6",
+        pickC: "3",
+        pickD: "1",
+        answer: "d"
+    }
+    {
+        question: "What tag would you use to add a image?",
+        pickA: "<img>",
+        pickB: "<div>",
+        pickC: "<p>",
+        pickD: "section",
+        answer: "a"
+    }
+    {
+        question: "What is href used for?",
+        pickA: "to add font",
+        pickB: "to store data",
+        pickC: "to change background color",
+        pickD: "to create link to another page",
+        answer: "d"
+    }
+    {
+        question: "How are Javascript files saved?",
+        pickA: ".css",
+        pickB: ".html",
+        pickC: ".js",
+        pickD: ".png",
+        answer: "c"
+    }
+
+];
