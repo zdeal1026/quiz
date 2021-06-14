c1df87bb35c40e86d48cda592c915e574dee08b2
Zdeal <jedimastershake@gmail.com>
2021-06-14 00:02:05 -0400

    debugging

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // grabbing ids
 var giveDirections = document.getElementById("directions");
-var questionsEl = document.getElementById("questions")
+var optionsEl = document.getElementById("options")
 var scoreEl = document.getElementById("score");
 var finalEl = document.getElementById("final");
 var endEl = document.getElementById("end");
@@ -11,7 +11,7 @@ var resultsEl = document.getElementById("results");
 var scoreBoardEl = document.getElementById("scoreBoard");
 var initialsEl = document.getElementById("initials");
 var leaderBoardEl = document.getElementById("leaderBoard");
-var endGame = document.getElementById("endButtons");
+var endbuttonsEl = document.getElementById("endButtons");
 var enterEl = document.getElementById("enter");
 var leaderScoreEl = document.getElementById("leaderScore");
 var alpha = document.getElementById("a");
@@ -73,9 +73,9 @@ var askQuestions = [
 ];
 
 //other variables
-var allQuestoins = askQuestions.length;
+var allQuestions = askQuestions.length;
 var currentOne = 0;
-var timeOnClock = 91;
+var timeOnClock = 46;
 var score = 0;
 var correct;
 var interval;
@@ -83,7 +83,7 @@ var interval;
 //
 function getQuestions() {
     endEl.style.display = "none";
-    if (currentOne === allQuestoins) {
+    if (currentOne === allQuestions) {
         return showResults();
     }
     var firstOne = askQuestions[currentOne];
@@ -107,17 +107,17 @@ function showQuestions() {
             clearInterval(interval);
             showResults();
         }
-    }); 1000;
-    questionsEl.style.display = "block";
+    }, 1000);
+    optionsEl.style.display = "block";
 }
 
 //start here
 function showResults() {
-    questionsEl.style.display = "none"
+    optionsEl.style.display = "none"
     endEl.style.display = "flex";
     clearInterval(interval);
     initialsEl.value = "";
-    finalEl.innerHTML = "You scored" + score + "out of" + allQuestoins.length + "correct!";
+    finalEl.innerHTML = "You got " + score + " out of 6 correct!";
 }
 
 //check parentheses here
@@ -130,14 +130,14 @@ enterEl.addEventListener("click", function highscore() {
         var savedScore = JSON.parse(localStorage.getItem("savedScore")) || [];
         var currentId = initialsEl.value.trim();
         var currentHigh = {
-            name: currentID,
+            name: currentId,
             score: score
         };
 
         endEl.style.display = "none";
         resultsEl.style.display = "flex";
         scoreBoardEl.style.display = "block";
-        endbuttonEl.style.display = "flex";
+        endbuttonsEl.style.display = "flex";
 
         savedScore.push(currentHigh);
         localStorage.setItem("savedScore", JSON.stringify(savedScore));
@@ -145,14 +145,14 @@ enterEl.addEventListener("click", function highscore() {
     }
 });
 
-//generats high scores
+//generates high scores
 function generateHigh() {
     leaderBoardEl.innerHTML = "";
     leaderScoreEl.innerHTML = "";
 
     var highScores = JSON.parse(localStorage.getItem("savedScore")) || {};
 
-    for (i = 0; i < highscores.length; i++) {
+    for (i = 0; i < highScores.length; i++) {
         var newName = document.createElement("li");
         var newScore = document.createElement("li");
         newName.textContent = highScores[i].name;
@@ -166,26 +166,26 @@ function generateHigh() {
 function showHigh() {
     giveDirections.style.display = "none"
     endEl.style.display = "none";
-    scoreBoardEl.style.display = "bolck";
-    endButtonsEl.style.display = "flex";
+    scoreBoardEl.style.display = "block";
+    endbuttonsEl.style.display = "flex";
     resultsEl.style.display = "flex";
 
     generateHigh();
 }
 
-//clears the highscores
+//clears the highscores click function on HTML
 function clearData() {
     window.localStorage.clear();
     leaderBoardEl.textContent = "";
     leaderScoreEl.textContent = "";
 }
 
-//resets the quiz
+//resets the quiz click function on HTML
 function repeatQuiz() {
     resultsEl.style.display = "none";
     endEl.style.display = "none";
     directions.style.display = "flex";
-    timeOnClock = 91;
+    timeOnClock = 46;
     score = 0;
     currentOne = 0;
 }
@@ -195,14 +195,14 @@ function repeatQuiz() {
 function checkAnswer(answer) {
     correct = askQuestions[currentOne].answer;
 
-    if (answer === correct && currentOne !== allQuestoins) {
+    if (answer === correct && currentOne !== allQuestions) {
         score++;
 
         alert("Correct");
         currentOne++;
         getQuestions();
 
-    } else if (answer !== correct && currentOne !== allQuestoins) {
+    } else if (answer !== correct && currentOne !== allQuestions) {
         alert("Incorrect");
         currentOne++;
         getQuestions();
